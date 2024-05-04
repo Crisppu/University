5e06cda24d089150c9f32cf08e1356faba81a721
Crisppu <criyattojin@gmail.com>
2024-05-04 11:10:24 -0600

    configuracion del CORS - dar acceso a las aplicaciones para hacer peticiones. como por ej. Get,Post,Update,Delete.

--- UniversityApiBackend/Program.cs
+++ UniversityApiBackend/Program.cs
@@ -23,6 +23,16 @@ aqui todos los controles como por ejemplo Studen
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//5. habilitar el CORS, configurar
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin(); //cualquier aplicacion tendra acceso a hacer peticiones
+        builder.AllowAnyMethod();//acceso a los get,post,get,update cualquier peticion
+        builder.AllowAnyHeader();//cualquier cabezera
+    });
+});
 
 var app = builder.Build();
 
@@ -38,5 +48,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+//6 decirle a la aplicación que use CORS
+app.UseCors("CorsPolicy");
 
 app.Run();
