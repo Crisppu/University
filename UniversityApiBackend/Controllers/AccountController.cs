45ccfb577fc5213bc36018b49bef23ddfa23eb46
Crisppu <criyattojin@gmail.com>
2024-05-09 17:21:55 -0600

    Se creo un modelo de Login y un Controller en blanco, ojo que no se creo un controllers con entity Franwork. se inicio desde cero

---
+++ UniversityApiBackend/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+//este controllaor se hizo desde cero
+
+namespace UniversityApiBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+    }
+}
---
+++ UniversityApiBackend/Models/DataModels/UserLogins.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class UserLogins
+    {
+        //ojo en la Controllers no se uso Entity franword sino mas bien  - Controlador de api en blanco
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
